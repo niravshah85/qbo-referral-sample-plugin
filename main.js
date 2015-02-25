1a5aed6d6fd7d5d3a0a8775c8f2332270cb1dc75
Nirav K. Shah <nirav_shah@intuit.com>
2015-02-25 13:15:23 -0800

    dummy payroll page so that we can test payroll event CMS pages are down for deployment

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 var sendemail = require('./routes/sendemail');
 var referral = require('./routes/referral');
 var dialog = require('./routes/dialog');
+var addpayroll = require('./routes/addpayroll');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use('/users', users);
 app.use("/sendemail", sendemail);
 app.use("/referral", referral);
 app.use("/dialog", dialog);
+app.use("/addpayroll", addpayroll);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
